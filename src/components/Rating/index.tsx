4dca89c3bb9fd8b4a77c26d7e8cc5f805d7cd9f0
juliorox666 <juliorox666@gmail.com>
2020-12-17 09:32:20 -0300

    Added empty components that i will need

---
+++ src/components/Rating/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Rating: React.FC = () => {
+  return <div></div>;
+};
+
+export default Rating;
--- src/containers/Content/index.tsx
+++ src/containers/Content/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import AppHeader from 'components/AppHeader';
+import AppContent from 'components/AppContent';
 import Home from 'pages/Home';
 import Details from 'pages/Details';
 
 const Content: React.FC = () => {
   return (
     <div className="appcontent">
-      <Switch>
-        <Route exact path={`/`} component={Home} />
-        <Route exact path={`/:id`} component={Details} />
-      </Switch>
+      <AppHeader />
+      <AppContent>
+        <Switch>
+          <Route exact path={`/`} component={Home} />
+          <Route exact path={`/:id`} component={Details} />
+        </Switch>
+      </AppContent>
     </div>
   );
 };
